44187f45f64ecd9a3c76a827f7d2422d0da1501c
harshil0073 <128502948+harshil0073@users.noreply.github.com>
2023-04-12 11:53:14 -0400

    updated

--- videoView1/videoView1/videoView1/videoViewController.swift
+++ videoView1/videoView1/videoView1/videoViewController.swift
@@ -6,12 +6,26 @@ import AVKit
 
 class videoViewController: UIViewController {
 
+    @IBOutlet weak var myVideoView: UIView!
+    var player : AVPlayer!
+    var avpController : AVPlayerViewController
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        startVideo()
 
      
     }
-    
+    func startVideo(){
+        let url = URL (string: "")
+        player = AVPlayer(url: url!)
+        avpController.player = player
+        avpController.view.frame = myVideoView.bounds
+        self.myVideoView.addSubview(avpController.view)
+        player.play()
+        
+    }
 
   
 
